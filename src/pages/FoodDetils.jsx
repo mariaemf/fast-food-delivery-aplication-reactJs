da8b0c08ac56702df3e613781a921c6d8fbeaac8
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-16 23:55:46 -0300

    creating routes to access the pages and changing the folder name to FoodDetails

--- src/pages/FoodDetils.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FoodDetils = () => {
-  return <div>FoodDetils</div>;
-};
-
-export default FoodDetils;
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import AllFoodes from "../pages/AllFoods";
+import Checkout from "../pages/Checkout";
+import Contact from "../pages/Contact";
+import FoodeDetails from "../pages/FoodDetails";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/foods" element={<AllFoodes />} />
+      <Route path="/foods/:id" element={<FoodeDetails />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 };
 
 export default Routers;

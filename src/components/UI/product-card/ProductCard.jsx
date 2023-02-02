4cac28766164ce09d1b1b7aa6c736aab48041a76
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-02 17:15:58 -0300

    added a dispatch variable when clicking on item choice items

--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../../Styles/product-card.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../../store/cartSlice";
 
 const ProductCard = (props) => {
   const { id, title, price, image01 } = props.item;
+  const dispatch = useDispatch();
+
+  const addToCard = () => {
+    dispatch(
+      cartActions.addItem({
+        id,
+        title,
+        image01,
+        price,
+      })
+    );
+  };
+
   return (
     <>
       <div className="product__item">
@@ -16,7 +31,9 @@ const ProductCard = (props) => {
           </h5>
           <div className="d-flex align-items-center justify-content-between">
             <span className="product__price">${price}</span>
-            <button className="addTOCart__btn">Add to Cart</button>
+            <button className="addTOCart__btn" onClick={addToCard}>
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>

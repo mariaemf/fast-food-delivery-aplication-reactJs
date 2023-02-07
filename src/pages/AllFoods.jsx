c4678907a72cacf79ffedb650f836cf320b1831b
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-07 20:58:42 -0300

    performing insertion of new images into the code, creation of new commonsection and allfoods css page, insertion of content and creation of commonsection and allfoods page

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/commom-section/CommonSection";
+
+import { Container, Row, Col } from "reactstrap";
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCard";
 
 const AllFoods = () => {
-  return <div>AllFoods</div>;
+  return (
+    <>
+      <Helmet title="All-Foods">
+        <CommonSection title="All Foods" />
+        <section>
+          <Container>
+            <Row>
+              <Col lg="6" md="6" sm="6">
+                <div className="search__widget">
+                  <input type="text" placeholder="I'm looking for..." />
+                  <span>
+                    <i class="ri-search-line"></i>
+                  </span>
+                </div>
+              </Col>
+              <Col lg="6" md="6" sm="6" className="mb-5">
+                <div className="sorting__widget text-end">
+                  <select>
+                    <option>Default</option>
+                    <option value="ascending">Alphabetically, A-Z</option>
+                    <option value="decending">Alphabetically, Z-A</option>
+                    <option value="high-price">High Price</option>
+                    <option value="low-price">Low Price</option>
+                  </select>
+                </div>
+              </Col>
+
+              {products.map((item) => (
+                <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                  <ProductCard item={item} />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </section>
+      </Helmet>
+    </>
+  );
 };
 
 export default AllFoods;

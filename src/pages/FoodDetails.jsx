6a428b8d0f1ba334ced4b248710d1eb92b0e603f
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-08 21:11:37 -0300

    Insertion of a new foodDetails page and insertion of its initial content

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import products from "../assets/fake-data/products";
+import { useParams } from "react-router-dom";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/common-section/commonSection";
+import { Container, Row, Col } from "reactstrap";
+import productImg from "../assets/images/product_01.1.jpg";
 
 const FoodDetails = () => {
-  return <div>FoodDetails</div>;
+  return (
+    <>
+      <Helmet title="Product-details">
+        <CommonSection title="product 01" />
+        <section>
+          <Container>
+            <Row>
+              <Col lg="6" md="6">
+                <div className="product__images"></div>
+              </Col>
+              <Col lg="6" md="6"></Col>
+            </Row>
+          </Container>
+        </section>
+      </Helmet>
+    </>
+  );
 };
 
 export default FoodDetails;

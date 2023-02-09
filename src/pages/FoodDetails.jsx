b27fe211c65436b15857140f57e92d92bcce242f
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-09 19:56:13 -0300

    realization of content within FoodDetails to detail them

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import products from "../assets/fake-data/products";
-import { useParams } from "react-router-dom";
+//import products from "../assets/fake-data/products";
+//import { useParams } from "react-router-dom";
 import Helmet from "../components/Helmet/Helmet";
-import CommonSection from "../components/common-section/commonSection";
+import CommonSection from "../components/UI/commom-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 import productImg from "../assets/images/product_01.1.jpg";
 
@@ -14,10 +14,36 @@ const FoodDetails = () => {
         <section>
           <Container>
             <Row>
-              <Col lg="6" md="6">
-                <div className="product__images"></div>
+              <Col lg="2" md="2">
+                <div className="product__images">
+                  <div className="img__item">
+                    <img src={productImg} alt="" className="w-50" />
+                  </div>
+                  <div className="img__item">
+                    <img src={productImg} alt="" className="w-50" />
+                  </div>
+                  <div className="img__item">
+                    <img src={productImg} alt="" className="w-50" />
+                  </div>
+                </div>
               </Col>
-              <Col lg="6" md="6"></Col>
+              <Col lg="4" md="4">
+                <div className="product__main-img">
+                  <img src={productImg} alt="" className="w-100" />
+                </div>
+              </Col>
+
+              <Col lg="6" md="6">
+                <div className="single__product-content">
+                  <h2 className="products__title">Pizza with mushroom</h2>
+                  <span className="products__price">$34</span>
+                  <p>
+                    Category: <span>Burger</span>
+                  </p>
+
+                  <button className="btn addTOCart__btn">Add to Cart</button>
+                </div>
+              </Col>
             </Row>
           </Container>
         </section>

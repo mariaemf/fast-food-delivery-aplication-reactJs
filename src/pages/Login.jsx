bbe996947b0871b2b1a91002b78adccfb088ed01
mariaemf <mariaeduardamartinsf@gmail.com>
2023-02-17 17:56:56 -0300

    site completion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/commom-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const loginNameRef = useRef();
+  const loginPasswordRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <Helmet title="Login">
@@ -13,19 +20,29 @@ const Login = () => {
           <Container>
             <Row>
               <Col lg="6" md="6" sm="12" className="m-auto text-center">
-                <form className="form mb-5">
+                <form className="form mb-5" onSubmit={submitHandler}>
                   <div className="form__group">
-                    <input type="text" placeholder="Email" required />
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      required
+                      ref={loginNameRef}
+                    />
                   </div>
                   <div className="form__group">
-                    <input type="password" placeholder="password" required />
+                    <input
+                      type="password"
+                      placeholder="password"
+                      required
+                      ref={loginPasswordRef}
+                    />
                   </div>
                   <button type="submit" className="addTOCart__btn">
                     Login
                   </button>
                 </form>
                 <Link to="/register">
-                  Already have an account? Create an account
+                  Don't have an account? Create an account
                 </Link>
               </Col>
             </Row>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useRef } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/commom-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const singupNameRef = useRef();
+  const singupPasswordRef = useRef();
+  const singEmailRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Helmet title="Singup">
+        <CommonSection title="Singup" />
+        <section>
+          <Container>
+            <Row>
+              <Col lg="6" md="6" sm="12" className="m-auto text-center">
+                <form className="form mb-5" onSubmit={submitHandler}>
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      placeholder="Full name"
+                      required
+                      ref={singupNameRef}
+                    />
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      required
+                      ref={singEmailRef}
+                    />
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      ref={singupPasswordRef}
+                    />
+                  </div>
+                  <button type="submit" className="addTOCart__btn">
+                    Sing Up
+                  </button>
+                </form>
+                <Link to="/register">Already have an account? Login</Link>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </Helmet>
+    </>
+  );
 };
 
 export default Register;
